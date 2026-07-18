cf7e3cf86104f4bbc65aa2b3719f67f079d416c3
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2026-07-18 12:04:45 +0530

    Debugging purposes for the github actions

--- tests/login-api.spec.ts
+++ tests/login-api.spec.ts
@@ -3,6 +3,10 @@ import {test, expect} from '@playwright/test';
 test.describe("Sample API Test", () => {
     test("Login API Test", async ({ request }) => {
 
+        //for debugging purpose, printing the environment variables
+        console.log("HOST: ", process.env.HOST);
+        console.log("USERNAME: ", process.env.USERNAME);
+
         const requestBody = {"identifier":process.env.USERNAME,"password":process.env.PASSWORD,"portal":"admin"};
         const response = await request.post(`${process.env.HOST}/api/auth/login`, {
             data: requestBody
