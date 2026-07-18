833d7ce00d52e24e01f1c875d01bd18052bb9183
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2026-07-18 11:58:06 +0530

    Update env variables to take account of github workflow

--- tests/login-api.spec.ts
+++ tests/login-api.spec.ts
@@ -3,8 +3,8 @@ import {test, expect} from '@playwright/test';
 test.describe("Sample API Test", () => {
     test("Login API Test", async ({ request }) => {
 
-        const requestBody = {"identifier":process.env.username,"password":process.env.password,"portal":"admin"};
-        const response = await request.post(`${process.env.host}/api/auth/login`, {
+        const requestBody = {"identifier":process.env.USERNAME,"password":process.env.PASSWORD,"portal":"admin"};
+        const response = await request.post(`${process.env.HOST}/api/auth/login`, {
             data: requestBody
         });
         expect(response.status()).toBe(200);
