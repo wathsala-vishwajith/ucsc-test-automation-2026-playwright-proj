e25acd109195f39e57488457c3485f430a5087fc
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2026-07-18 14:11:48 +0530

    Refactor login API test to improve structure and readability

--- tests/login-api.spec.ts
+++ tests/login-api.spec.ts
@@ -1,18 +1,31 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test.describe("Sample API Test", () => {
-    test("Login API Test", async ({ request }) => {
+  let token: string;
 
-        //for debugging purpose, printing the environment variables
-        console.log("HOST: ", process.env.HOST);
-        console.log("USERNAME: ", process.env.USERNAME);
-
-        const requestBody = {"identifier":process.env.USERNAME,"password":process.env.PASSWORD,"portal":"admin"};
-        const response = await request.post(`${process.env.HOST}/api/auth/login`, {
-            data: requestBody
-        });
-        expect(response.status()).toBe(200);
-        const responseBody = await response.json();
-        expect(responseBody).toHaveProperty('token');
+  test.beforeEach(async ({ request }) => {
+    const loginResponse = await request.post(`${process.env.HOST}/api/auth/login`, {
+      data: {
+        identifier: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        portal: "admin",
+      },
     });
+    expect(loginResponse.status()).toBe(200);
+    const loginBody = await loginResponse.json();
+    expect(loginBody).toHaveProperty('token');
+    token = loginBody.token;
+  });
+
+  test("Get User Profile API Test", async ({ request }) => {
+    const response = await request.get(`${process.env.HOST}/api/admin/profile`, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+    });
+    expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    expect(responseBody).toHaveProperty('profile');
+    expect(responseBody.profile).toHaveProperty('username', process.env.USERNAME);
+  });
 });
